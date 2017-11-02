a6f326119a1c0405ff234d3f87883173832d975e
Myke Bates <contact@mykebates.com>
2017-11-01 22:11:53 -0500

    testing image path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { HomePage } from '../pages/home/home';
+//import { HomePage } from '../pages/home/home';
 import {LoginPage} from "../pages/login/login";
 
 @Component({
