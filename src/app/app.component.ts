803c1e1cb28714e8bd6f65851e8f2f29de6d2ecb
Myke Bates <contact@mykebates.com>
2017-11-02 23:13:00 -0500

    ugggh. all sorts of stuff. login page looking pretty slick now, though.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,25 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {Component} from '@angular/core';
+import {Platform} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 //import { HomePage } from '../pages/home/home';
 import {LoginPage} from "../pages/login/login";
 
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyApp {
-  //rootPage:any = HomePage;
-  rootPage:any = LoginPage;
+    //rootPage:any = HomePage;
+    rootPage: any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
+    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            //statusBar.styleDefault();
+            statusBar.hide();
+            splashScreen.hide();
+        });
+    }
 }
 
--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -1,13 +1,27 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { LoginPage } from './login';
+import {NgModule} from '@angular/core';
+import {IonicPageModule, Platform} from 'ionic-angular';
+import {LoginPage} from './login';
+import {StatusBar} from "@ionic-native/status-bar";
 
 @NgModule({
-  declarations: [
-    LoginPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(LoginPage),
-  ],
+    declarations: [
+        LoginPage,
+    ],
+    imports: [
+        IonicPageModule.forChild(LoginPage),
+    ]
 })
-export class LoginPageModule {}
+export class LoginPageModule {
+    constructor(platform: Platform, statusBar: StatusBar) {
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            //statusBar.hide();
+        });
+        //statusBar.hide();
+        //this.statusBar.hide();
+        //this.statusBar.overlaysWebView(false);
+    }
+
+
+}
