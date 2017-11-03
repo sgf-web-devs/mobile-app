09d6383ec3abaf0d5bd123f0c6aa076f5e4b456d
Myke Bates <contact@mykebates.com>
2017-11-02 23:42:51 -0500

    whoops. did not mean to put this in the module.

--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
-import {IonicPageModule, Platform} from 'ionic-angular';
+import {IonicPageModule, NavController, Platform} from 'ionic-angular';
 import {LoginPage} from './login';
-import {StatusBar} from "@ionic-native/status-bar";
 
 @NgModule({
     declarations: [
@@ -12,16 +11,5 @@ import {StatusBar} from "@ionic-native/status-bar";
     ]
 })
 export class LoginPageModule {
-    constructor(platform: Platform, statusBar: StatusBar) {
-        platform.ready().then(() => {
-            // Okay, so the platform is ready and our plugins are available.
-            // Here you can do any higher level native things you might need.
-            //statusBar.hide();
-        });
-        //statusBar.hide();
-        //this.statusBar.hide();
-        //this.statusBar.overlaysWebView(false);
-    }
-
 
 }
