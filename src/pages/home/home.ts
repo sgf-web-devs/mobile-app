ce04b73707c9af128ad2f666845cbe3f3f1bf9b3
Levi Zitting <levizitting5663@gmail.com>
2017-11-09 17:58:29 -0600

    Refactoring to allow attendee-list-item component to accept data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+    attendees = [
+        {
+            name: 'Myke Bates',
+            description: 'Full Stack Dev',
+            checkInTime: '1m',
+            profileImg: 'assets/imgs/profile.jpg'
+        },
+        {
+            name: 'Levi Zitting',
+            description: 'Front End Dev',
+            checkInTime: '4m',
+            profileImg: 'assets/imgs/profile.jpg'
+        }
+    ]
+
     constructor(public navCtrl: NavController) {
 
     }
