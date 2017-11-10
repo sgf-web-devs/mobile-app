62cacc273a0b0b255badfaeb6f31b5a4cd05caf9
Levi Zitting <levizitting5663@gmail.com>
2017-11-09 18:24:37 -0600

    Adding more mock data to attendee service provider

--- src/providers/attendee/attendee.ts
+++ src/providers/attendee/attendee.ts
@@ -9,32 +9,88 @@ import { of } from 'rxjs/observable/of';
 @Injectable()
 export class AttendeeProvider {
 
-    private attendees = [
-        {
-            name: 'Myke Bates',
-            description: 'Full Stack Dev',
-            checkInTime: '1m',
-            profileImg: 'assets/imgs/profile.jpg'
-        },
-        {
-            name: 'Levi Zitting',
-            description: 'Front End Dev',
-            checkInTime: '4m',
-            profileImg: 'assets/imgs/profile.jpg'
-        },
-        {
-            name: 'John Doe',
-            description: 'Back End Dev',
-            checkInTime: '10m',
-            profileImg: 'assets/imgs/profile.jpg'
-        }
-    ];
+    private attendees = [{
+        "name": "Andria Fish",
+        "description": "Full Stack Dev",
+        "checkInTime": "1m",
+        "profileImg": "assets/imgs/profile.jpg"
+    }, {
+        "name": "Kimberly Wolseley",
+        "description": "Full Stack Dev",
+        "checkInTime": "1m",
+        "profileImg": "assets/imgs/profile.jpg"
+    }, {
+        "name": "Bettye Hardington",
+        "description": "Full Stack Dev",
+        "checkInTime": "1m",
+        "profileImg": "assets/imgs/profile.jpg"
+    }, {
+        "name": "Vergil MacFadzan",
+        "description": "Back End Dev",
+        "checkInTime": "1m",
+        "profileImg": "assets/imgs/profile.jpg"
+    }, {
+        "name": "Sauncho Kremer",
+        "description": "Full Stack Dev",
+        "checkInTime": "1m",
+        "profileImg": "assets/imgs/profile.jpg"
+    }, {
+        "name": "Bartholomew Worster",
+        "description": "Full Stack Dev",
+        "checkInTime": "1m",
+        "profileImg": "assets/imgs/profile.jpg"
+    }, {
+        "name": "Hermann Byass",
+        "description": "Front End Dev",
+        "checkInTime": "1m",
+        "profileImg": "assets/imgs/profile.jpg"
+    }, {
+        "name": "Bernie Moncey",
+        "description": "Front End Dev",
+        "checkInTime": "1m",
+        "profileImg": "assets/imgs/profile.jpg"
+    }, {
+        "name": "Skippy Loudian",
+        "description": "Front End Dev",
+        "checkInTime": "1m",
+        "profileImg": "assets/imgs/profile.jpg"
+    }, {
+        "name": "Mufi Sallenger",
+        "description": "Full Stack Dev",
+        "checkInTime": "1m",
+        "profileImg": "assets/imgs/profile.jpg"
+    }, {
+        "name": "Hank Enriques",
+        "description": "Front End Dev",
+        "checkInTime": "1m",
+        "profileImg": "assets/imgs/profile.jpg"
+    }, {
+        "name": "Maressa Doudney",
+        "description": "Full Stack Dev",
+        "checkInTime": "1m",
+        "profileImg": "assets/imgs/profile.jpg"
+    }, {
+        "name": "Meier Otley",
+        "description": "Back End Dev",
+        "checkInTime": "1m",
+        "profileImg": "assets/imgs/profile.jpg"
+    }, {
+        "name": "Adria Sotworth",
+        "description": "Front End Dev",
+        "checkInTime": "1m",
+        "profileImg": "assets/imgs/profile.jpg"
+    }, {
+        "name": "Thurstan Gerrans",
+        "description": "Back End Dev",
+        "checkInTime": "1m",
+        "profileImg": "assets/imgs/profile.jpg"
+    }];
 
     constructor() {
     }
 
     getAttendees() {
-        return of( this.attendees );
+        return of(this.attendees);
     }
 
 }
